0602e34962205f8422fcc9004395e05096ad2c6d
mateuszKrawczak <mateuszkrawczak19@gmail.com>
2021-12-02 19:57:58 +0100

    menu and pause menu

---
+++ Assets/Scripts/PauseMenuActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuActions : MonoBehaviour
+{
+
+    public GameObject pauseMenuUI;
+
+    public void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void RedirectToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
